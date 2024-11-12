22b8bbb2fd7e75de686401614a7e788a8e1e1fd0
nnnn2cat <93114535+nnnn2cat@users.noreply.github.com>
2024-11-12 21:50:23 +0900

    Monochrome: Clean up work directory before conversion

--- wqv_monochrome/convert.bat
+++ wqv_monochrome/convert.bat
@@ -1,6 +1,9 @@
 ::Start by dividing the pdb into individual files
 par x pdb/wqvlinkdb.pdb
 
+::clean work directory
+del work\*.pdr*
+
 ::move all extracted files to source directory
 for /r  %%x in (*.pdr) do move "%%x" "work"
 ::for /r  %%x in (*.pdr) do move "%%x" work
